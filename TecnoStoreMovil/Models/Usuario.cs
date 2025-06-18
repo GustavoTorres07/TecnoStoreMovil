8b2b589c4ba0ff5cc7d09908482023097f422d2c
Gustavo Torres <gtreal62@gmail.com>
2025-06-18 20:23:58 -0300

    Agregar archivos de proyecto.

---
+++ TecnoStoreMovil/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnoStoreMovil.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } 
+        public string Apellido { get; set; } 
+        public string Dni { get; set; } 
+        public string Email { get; set; } 
+        public string Celular { get; set; }
+        public string Clave { get; set; } 
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public string FotoUsuario { get; set; } 
+        public bool Activo { get; set; }
+        public RolUsuario Rol { get; set; }
+
+    }
+}
---
+++ TecnoStoreMovil/Services/UsuarioService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TecnoStoreMovil.Models;
+
+namespace TecnoStoreMovil.Services
+{
+    public class UsuarioService
+    {
+        private readonly List<Usuario> usuarios;
+
+        public UsuarioService()
+        {
+            usuarios = new List<Usuario>()
+            {
+                new Usuario { Id = 1,
+                    Nombre = "Gustavo",
+                    Apellido = "Torres",
+                    Dni = "39054025",
+                    Email = "gtreal62@gmail.com",
+                    Celular = "+54 9 2954-216751",
+                    Clave = "HalaMadrid7",
+                    FechaRegistro = DateTime.Now,
+                    FotoUsuario = "img/usuario_default.jpg",
+                    Rol = RolUsuario.Administrador,
+                    Activo = true
+
+                },
+                new Usuario { Id = 2,
+                    Nombre = "Ana",
+                    Apellido = "Perez",
+                    Dni = "24584123",
+                    Email = "anaperez@gmail.com",
+                    Celular = "+54 9 2954-159634",
+                    Clave = "anaperez1",
+                    FechaRegistro = DateTime.Now,
+                    FotoUsuario = "img/usuario_default.jpg",
+                    Rol = RolUsuario.Cliente,   
+                    Activo = true
+                },
+                new Usuario { Id = 3,
+                    Nombre = "Luis",
+                    Apellido = "Garcia",
+                    Dni = "45519849",
+                    Email = "anaperez@gmail.com",
+                    Celular = "+54 9 2954-202541",
+                    Clave = "anaperez1", 
+                    FechaRegistro = DateTime.Now,
+                    FotoUsuario = "img/usuario_default.jpg",
+                    Rol = RolUsuario.Cliente,   
+                    Activo = false
+                }
+            };
+        }
+
+        public List<Usuario> GetUsuarios() => usuarios;
+
+        public Usuario? GetUsuario(int Id) => usuarios.FirstOrDefault(u => u.Id == Id);
+
+        public Usuario? GetUsuarioPorEmail(string email) => usuarios.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+
+        public void AddUsuario(Usuario usuario)
+        {
+            usuario.Id = usuarios.Any() ? usuarios.Max(u => u.Id) + 1 : 1;
+            usuarios.Add(usuario);
+        }
+
+        public void UpdateUsuario(Usuario usuario)
+        {
+            var index = usuarios.FindIndex(u => u.Id == usuario.Id);
+            if (index >= 0) usuarios[index] = usuario;
+        }
+        public void DeleteUsuario(int id)
+        {
+            var usuario = GetUsuario(id);
+            if (usuario != null) usuarios.Remove(usuario);
+        }
+        public Usuario? ValidarLogin(string email, string clave)
+        {
+            var user = GetUsuarioPorEmail(email);
+            if (user != null && user.Clave == clave && user.Activo)
+                return user;
+            return null;
+        }
+    }
+}
